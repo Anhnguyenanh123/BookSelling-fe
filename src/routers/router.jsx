51b4d59d8c6b9bc39e63e3319adff3e1290e9665
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2024-12-22 00:43:39 +0700

    checkout and cart page

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -3,6 +3,8 @@ import App from "../App";
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/books/CartPage";
+import CheckOut from "../pages/books/CheckOut";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +30,14 @@ const router = createBrowserRouter([
       {
         path: "/orders",
         element: <div>Orders</div>,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/checkout",
+        element: <CheckOut />,
       },
     ],
   },
