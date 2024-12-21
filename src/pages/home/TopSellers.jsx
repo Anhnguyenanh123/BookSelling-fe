8213dc5b2e3ffb9dfc641e86d4e9c211c91e1fb8
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2024-12-21 16:21:25 +0700

    top seller

--- src/pages/home/TopSellers.jsx
+++ src/pages/home/TopSellers.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const categories = [
   "Choose a genre",
@@ -12,20 +17,18 @@ const TopSellers = () => {
   const [books, setBooks] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
 
-  const filteredBooks =
-    selectedCategory === "Choose a genre"
-      ? books
-      : books.filter(
-          (book) => book.category === selectedCategory.toLowerCase()
-        );
-
   useEffect(() => {
     fetch("books.json")
       .then((response) => response.json())
       .then((data) => setBooks(data));
   }, []);
 
-  console.log(filteredBooks);
+  const filteredBooks =
+    selectedCategory === "Choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
 
   return (
     <div className="py-10">
@@ -45,9 +48,39 @@ const TopSellers = () => {
           ))}
         </select>
       </div>
-      {filteredBooks.map((book, index) => (
-        <div>{book.title}</div>
-      ))}
+
+      <Swiper
+        navigation={true}
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {filteredBooks.length > 0 &&
+          filteredBooks.map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 };
