c6b8e2e7e160371010531a2bf3ba55e4c7a4f1a6
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-01-03 21:49:57 +0700

    remove

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,15 +64,6 @@ const Navbar = () => {
           <Link to="/">
             <FaBars className="size-6" />
           </Link>
-          {/* Search Input */}
-          <div className="relative sm:w-72 w-40 space-x-2">
-            <FaSearch className="absolute inline-block left-3 inset-y-2" />
-            <input
-              type="text"
-              placeholder="Search"
-              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
-            />
-          </div>
         </div>
         {/* Right side */}
         <div className="relative flex items-center md:space-x-3 space-x-2">
